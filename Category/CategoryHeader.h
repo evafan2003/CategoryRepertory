ba05e67d9eb1b145770cfbb1730219dfb5ce3758
hefei <hefei@dycd.com>
2018-03-06 17:08:14 +0800

    重新整理

---
+++ Category/CategoryHeader.h
@@ -0,0 +1,35 @@
+//
+//  CategoryHeader.h
+//  
+//
+//  Created by hefei on 2018/3/6.
+//
+//第三方类
+#import "MBProgressHUD+Add.h"
+//数据类型工具类
+#import "NSArray+Category.h"
+#import "NSMutableArray+Category.h"
+#import "NSAttributedString+Category.h"
+#import "NSData+Base64.h"
+#import "NSDate+Agenda.h"
+#import "NSDate+Formatter.h"
+#import "NSDictionary+Category.h"
+#import "NSMutableAttributedString+Category.h"
+#import "NSMutableDictionary+Category.h"
+#import "NSString+Category.h"
+#import "NSString+DisplayTime.h"
+#import "NSString+MD5.h"
+#import "NSString+Predicate.h"
+#import "NSTimer+Category.h"
+//视图相关类
+#import "UIButton+Category.h"
+#import "UIColor+BGHexColor.h"
+#import "UIControl+ResponseTime.h"
+#import "UIImage+Category.h"
+#import "UIMenuItem+Category.h"
+#import "UINavigationController+FDFullscreenPopGesture.h"
+#import "UINavigationController+StatusBarStyle.h"
+#import "UITableViewCell+CellAnimation.h"
+#import "UIView+Animation.h"
+#import "UIView+BGFrame.h"
+#import "UIView+Category.h"
